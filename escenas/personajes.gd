cf62b36cc5749b0ad5843c1954f69ce015b7fb5e
Mapachana <anabuenrua99@gmail.com>
2024-01-27 11:45:26 +0100

    Logica de personaje #1

--- escenas/personajes.gd
+++ escenas/personajes.gd
@@ -6,20 +6,44 @@ const SPEED = 300.0
 @export var puntos: PackedVector2Array
 
 var target_index
+var infectado
+var radio
+var velocidad_giro
+var angulo
+
 
 func _ready():
-		target_index = 0
-
-	
-	
+	target_index = 0
+	infectado = true
+	radio = 1000000
+	velocidad_giro = 10
+	angulo = 0
 
 func _physics_process(delta):
-
-	var target = puntos[target_index]
-
-	if (position.distance_to(target) < 1) and (target_index < (puntos.size()-1)) :
-		target_index += 1
-		target = puntos[target_index]
-		velocity = (target - position).normalized() * SPEED
 	
+	var target
+	if (!infectado):
+		target = puntos[target_index]
+
+		if (position.distance_to(target) < 1) and (target_index < (puntos.size()-1)) :
+			print("YA")
+			print(position.distance_to(target))
+			target_index += 1
+			target = puntos[target_index]
+
+	else:
+		angulo = fmod(angulo + (velocidad_giro * delta), 2 * PI)
+		target = Vector2(cos(angulo), sin(angulo))*radio
+		
+	velocity = (target - position).normalized() * SPEED	
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body):
+	if body.infectado:
+		# Me infecto yo
+		self.infectado = true;
+		
+		# Si al infectarme yo hay otros cerca mia, los infecto
+		for bodies in $Area2D.get_overlapping_bodies(): #This one SHOULD get all the bodies in the area.
+			bodies.infectado = true;
