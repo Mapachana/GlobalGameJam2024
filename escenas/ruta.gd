ba1275dca7f64b51490ee9842920f09ddf2ce101
Juanjo Ramos <jjramos-dev@users.noreply.github.com>
2024-01-27 18:10:21 +0100

    Dinámica de seguimiento de ruta

--- escenas/ruta.gd
+++ escenas/ruta.gd
@@ -3,15 +3,29 @@ extends Node2D
 
 var ruta=[]
 
+const intervalo_puntos=10
+const offset=5
+
 enum {sin_iniciar,iniciado,final}
 
 var estado=sin_iniciar
+var curva:Curve2D
+var progreso=0
+var primer_punto_ruta=null
+var ultimo_punto=null
+
+var total_pasos=-1
+var indice_paso=0
+var siguiente_objetivo=null
+
+
+@export
+var velocidad=5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
+	pass
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("pinchar"):
@@ -20,15 +34,52 @@ func _process(delta):
 		seguir_ruta()
 	elif Input.is_action_just_released("pinchar"):
 		parar_ruta()	
-
+		
+	if estado==final:
+		if siguiente_objetivo!=null:
+			if $MuelaParada.position.distance_to(siguiente_objetivo)<offset:
+				indice_paso=clamp(indice_paso+1,0,total_pasos-1)
+				siguiente_objetivo=$Line2D.get_point_position(indice_paso)
+			$MuelaParada.position=$MuelaParada.position+(siguiente_objetivo-$MuelaParada.position).normalized()*velocidad
+			
+		#$Path2D/PathFollow2D.set_progress(progreso)
+		#progreso=clamp(progreso+delta,0,1.0)
+		
 func comenzar_ruta():
-	ruta=[get_viewport().get_mouse_position()]
+	var punto=get_viewport().get_mouse_position()
+	ruta=[punto]
+	estado=sin_iniciar
+	$Line2D.add_point(punto)
+	$MuelaParada.global_position=punto
+	curva=$Path2D.get_curve()
+	primer_punto_ruta=punto
+	curva.add_point(Vector2.ZERO)
+	ultimo_punto=punto
+	#$Path2D.points.add_point(Vector2.ZERO)
 	
 func seguir_ruta():
-	pass
-
+	var punto=get_viewport().get_mouse_position()
+	
+	if punto.distance_to(ultimo_punto)>=intervalo_puntos:	
+		ruta.append(punto)
+		estado=iniciado
+		$Line2D.add_point(punto)
+		curva.add_point(punto-primer_punto_ruta)
+		#$Path2D.points.add_point(punto-primer_punto_ruta)
+		ultimo_punto=punto
+	
 func parar_ruta():
-	pass
+	var punto=get_viewport().get_mouse_position()
+	ruta.append(punto)
+	estado=final
+	$Line2D.add_point(punto)
+	curva.add_point(punto-primer_punto_ruta)
+	#$Path2D.points.add_point(punto-primer_punto_ruta)
+		
+	$Path2D.set_curve(curva)
+	total_pasos=$Line2D.get_point_count()
+	indice_paso=0
+	siguiente_objetivo=$Line2D.get_point_position(indice_paso)
 	
 func _on_input_event(viewport, event, shape_idx):
 	pass
