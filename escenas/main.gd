0f93342d86353e21b1ba90ad6f9d5b30006e831c
Mapachana <anabuenrua99@gmail.com>
2024-01-28 03:22:23 +0100

    añado logica fin partida

--- escenas/main.gd
+++ escenas/main.gd
@@ -2,19 +2,49 @@ extends Node2D
 
 #signal sumarpunto
 # Called when the node enters the scene tree for the first time.
+
+const TIEMPO_MAXIMO = 4
+
+var personajes_escena
+var contador
+
 func _ready():
 	
 	add_to_group("ventana")
+	
+	contador = 0
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	contador += delta
+	
+	if contador >= TIEMPO_MAXIMO:
+		print("FIN DE TIEMPO")
 	pass
 
 func mostrar_puntuacion(punt):
 	var puntos_pantalla = "Teeth infected: %s"%punt
 	$Control/Label.text = puntos_pantalla
+	
+	personajes_escena = get_tree().get_nodes_in_group("personajes")
+	
+	var i = 0
+	var todos_muertos = true
+	while i < len(personajes_escena) and todos_muertos:
+		if !personajes_escena[i].muerto and !personajes_escena[i].infectado:
+			todos_muertos = false
+		i += 1
+	
+	if todos_muertos:
+		print("GANAS")
+		pass
+	else:
+		print("FINNN")
+		contador = 0
+		
 
 
 func _on_sumarpunto():
