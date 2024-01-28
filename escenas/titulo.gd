d5703a310117b123c83181156ac73c2a1e18b253
Mapachana <anabuenrua99@gmail.com>
2024-01-28 03:21:49 +0100

    Añado pantallas menu #8

---
+++ escenas/titulo.gd
@@ -0,0 +1,14 @@
+extends TextureRect
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	pass
+	
+
+func _on_button_pressed():
+	Globales.goto_scene("res://escenas/main.tscn")
+	pass # Replace with function body.
